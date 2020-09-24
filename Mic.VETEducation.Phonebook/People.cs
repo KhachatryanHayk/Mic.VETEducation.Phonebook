f3126ed1a64373e095ab586f42fa1521b5ee5375
Hayk Khachatryan <23haykgol@gmail.com>
2020-09-24 17:11:32 +0400

    Add project files.

---
+++ Mic.VETEducation.Phonebook/People.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+
+namespace Mic.VETEducation.Phonebook
+{
+    class People : IEnumerable
+    {
+        public string Nickname { get; set; }
+        public int PhoneNumber { get; set; }
+        public People Next;
+
+        public People AddPeople(string Nickname, int PhoneNumber)
+        {
+                if (Next == null)
+                {
+                    Next = new People { Nickname = Nickname, PhoneNumber = PhoneNumber };
+                    return Next;
+                }
+
+                Next.AddPeople(Nickname, PhoneNumber);
+                return Next;
+        }
+        public void RemovePeople(string Nickname)
+        {
+            //...
+        }
+        public void GetPersonNumber(string Nickname)
+        {
+            People Temp = this;
+            GetByKey(Nickname, ref Temp);
+
+            Console.WriteLine($"{Nickname} : +374{Temp.PhoneNumber}");
+        }
+        public void GetPeopleList()
+        {
+            Console.WriteLine($"{Nickname} : +374{PhoneNumber}");
+            if (Next != null)
+                Next.GetPeopleList();
+        }
+        private void GetByKey(string Nickname, ref People people)
+        {
+            if (this.Nickname == Nickname)
+            {
+                people = this;
+            }
+            else
+            {
+                Next.GetByKey(Nickname, ref people);
+            }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+        private class Enumerator : IEnumerator
+        {
+            private People _root;
+
+
+            public Enumerator(People root)
+            {
+                _root = root;
+            }
+
+            public object Current { get; private set; }
+
+            public bool MoveNext()
+            {
+                if (_root == null)
+                    return false;
+
+                Current = _root.Nickname;
+                _root = _root.Next;
+
+                return true;
+            }
+
+            public void Reset()
+            {
+                _root = null;
+            }
+        }
+    }
+}
---
+++ Mic.VETEducation.Phonebook/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Mic.VETEducation.Phonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary___
+            //var phonebook = new Phonebook();
+
+            //phonebook.AddPeople("Hayk Khachatryan", 123456789);
+            //phonebook.AddPeople("Gor", 123456789);
+            //phonebook.AddPeople("Vazgen", 123456789);
+            //phonebook.AddPeople("Miqayel", 123456789);
+            //phonebook.AddPeople("Arame", 123456789);
+            //phonebook.AddPeople("Albert", 123456789);
+            //phonebook.AddPeople("Edmon", 123456789);
+            //phonebook.AddPeople("Hayk", 123456789);
+            //phonebook.AddPeople("Hayk", 123456789);
+
+            //phonebook.GetPersonNumber("Hayk");
+            //phonebook.GetPeopleList();
+            //Console.WriteLine();
+            //phonebook.RemovePeople("Hayk Khachatryan");
+            //phonebook.RemovePeople("Albert");
+            //phonebook.GetPeopleList();
+
+
+            //IEnumerable___
+            var root = new People { Nickname = "Hayk", PhoneNumber = 123456789 };
+
+            root.AddPeople("Hayk Khachatryan", 123456789);
+            root.AddPeople("Gor", 123456789);
+            root.AddPeople("Vazgen", 123456789);
+            root.AddPeople("Miqayel", 123456789);
+            root.AddPeople("Arame", 123456789);
+            root.AddPeople("Albert", 123456789);
+            root.AddPeople("Edmon", 123456789);
+            root.AddPeople("Hayk", 123456789);
+
+            root.GetPersonNumber("Hayk Khachatryan");
+            Console.WriteLine();
+            root.GetPeopleList();
+            Console.WriteLine();
+            //root.RemovePeople("Hayk Khachatryan");
+            //root.RemovePeople("Albert");
+            //root.GetPeopleList();
+        }
+    }
+}
