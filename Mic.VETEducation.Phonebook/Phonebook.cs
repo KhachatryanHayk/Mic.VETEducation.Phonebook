a8fcc92fab70e90eff49463a29cb8ccf3a5eec50
Hayk Khachatryan <23haykgol@gmail.com>
2020-09-26 14:06:20 +0400

    SiriuS

---
+++ Mic.VETEducation.Phonebook/Phonebook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mic.VETEducation.Phonebook
+{
+    class Phonebook
+    {
+        private readonly Dictionary<string, int> _phoneBook = new Dictionary<string, int>();
+
+        public void AddPeople(string nickname, int phoneNumber)
+        {
+            try
+            {
+                _phoneBook.Add(nickname, phoneNumber);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"You already have a person named {nickname} in your phone book.");
+            }
+        }
+        public void RemovePeople(string nickname)
+        {
+            _phoneBook.Remove(nickname);
+        }
+        public void PrintPersonNumber(string nickname)
+        {
+            try
+            {
+                Console.WriteLine($"{nickname} : +374{_phoneBook[nickname]}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"There is no man named {nickname} in your phone book");
+            }
+        }
+        public void PrintPeopleList()
+        {
+            foreach (var people in _phoneBook)
+            {
+                Console.WriteLine($"{people.Key} : +374{people.Value}");
+            }
+        }
+    }
+}
--- Mic.VETEducation.Phonebook/Program.cs
+++ Mic.VETEducation.Phonebook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Mic.VETEducation.Phonebook
 {
@@ -7,7 +8,7 @@ namespace Mic.VETEducation.Phonebook
         static void Main(string[] args)
         {
             //Dictionary___
-            var phonebook1 = new Phonebook1();
+            var phonebook1 = new Phonebook();
 
             phonebook1.AddPeople("Hayk Khachatryan", 11111111);
             phonebook1.AddPeople("Gor", 22222222);
@@ -20,48 +21,19 @@ namespace Mic.VETEducation.Phonebook
             phonebook1.AddPeople("Hayk", 99999999);
             Console.WriteLine();
 
-            phonebook1.GetPersonNumber("Hayk");
-            phonebook1.GetPersonNumber("UnavailableName");
+            phonebook1.PrintPersonNumber("Hayk");
+            phonebook1.PrintPersonNumber("UnavailableName");
             Console.WriteLine();
 
             Console.WriteLine("Old List");
-            phonebook1.GetPeopleList();
+            phonebook1.PrintPeopleList();
             Console.WriteLine();
 
             phonebook1.RemovePeople("Hayk Khachatryan");
             phonebook1.RemovePeople("Albert");
             phonebook1.RemovePeople("UnavailableName");
             Console.WriteLine("New List");
-            phonebook1.GetPeopleList();
-
-            Console.WriteLine(new string('-',23));
-            //IEnumerable___
-
-            var phonebook2 = new Phonebook2 { Nickname = "Hayk Khachatryan", PhoneNumber = 11111111 };
-
-            phonebook2.AddPeople("Gor", 22222222);
-            phonebook2.AddPeople("Vazgen", 33333333);
-            phonebook2.AddPeople("Miqayel", 44444444);
-            phonebook2.AddPeople("Arame", 55555555);
-            phonebook2.AddPeople("Albert", 66666666);
-            phonebook2.AddPeople("Edmon", 77777777);
-            phonebook2.AddPeople("Hayk", 88888888);
-            phonebook2.AddPeople("Hayk", 99999999);
-            Console.WriteLine();
-
-            phonebook2.GetPersonNumber("Hayk");
-            phonebook2.GetPersonNumber("UnavailableName");
-            Console.WriteLine();
-
-            Console.WriteLine("Old List");
-            phonebook2.GetPeopleList();
-            Console.WriteLine();
-
-            phonebook2.RemovePeople("Hayk Khachatryan");
-            phonebook2.RemovePeople("Albert");
-            phonebook2.RemovePeople("UnavailableName");
-            Console.WriteLine("New List");
-            phonebook2.GetPeopleList();
+            phonebook1.PrintPeopleList();
 
         }
     }
